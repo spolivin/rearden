25ac0ac8beb6132b14d7b7fcaac53bf671be1c8d
Sergey Polivin <s.polivin@gmail.com>
2023-12-10 19:51:03 +0100

    style(init): Added a docstring for package description

--- src/rearden/__init__.py
+++ src/rearden/__init__.py
@@ -1,1 +1,21 @@
-__version__ = "0.0.2"
+"""Python package for Data Science and Machine Learning.
+
+-----------------------------------------------------
+
+Library of useful data structures for data preprocessing (spatial/time-series),
+vizualizations and grid search.
+
+Python modules included in the package:
+
+- `preprocessings.py` => Data preprocessing tools
+- `vizualizations.py` => Data visualization tools
+- `time_series.py` => Data tools for time-series analysis
+- `grid_search.py` => Extra data structures for grid search
+- `decorators.py` => Helpful decorators for functions
+- `metrics.py` => Additional helpful metrics
+"""
+
+# Author: Sergey Polivin <s.polivin@gmail.com>
+# License: MIT License
+
+__version__ = "0.0.3"
